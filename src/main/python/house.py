0b0519c6d0786c3b7f94c0a05a61602f240dec15
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-07-14 12:49:21 -0400

    added lunch update

--- src/main/python/house.py
+++ src/main/python/house.py
@@ -4,14 +4,16 @@ class House:
         self.bedroom = bedroom
 
     def bake_cookies(self):
-        self.kitchen.setOven(350)
+        self.kitchen.set_oven(350)
+        self.kitchen.set_oven(150)
+        self.kitchen.set_oven(650)
 
 
 class Kitchen:
     def __init__(self):
         pass
 
-    def setOven(self, temp):
+    def set_oven(self, temp):
         print "connecting to a real oven"
         print "there's a lot going on here"
         print "raising temp to " + temp
--- src/unittest/python/house_tests.py
+++ src/unittest/python/house_tests.py
@@ -1,16 +1,18 @@
 import unittest
 
 from hamcrest import *
-
+from mockito import mock, unstub, verify
 from house import House
 
-#Test Double
+
+# Test Double - Stub - Canned Answer
 class KitchenStub:
     def __init__(self):
-       self.temp = 0
+        self.temp = 0
 
-    def setOven(self, temp):
-       self.temp = temp
+    def set_oven(self, temp):
+        self.temp = temp
+
 
 class HouseTest(unittest.TestCase):
     """Think of the simplest thing first
@@ -19,9 +21,19 @@ class HouseTest(unittest.TestCase):
 
     """
 
+    # @staticmethod
+    # def test_house_set_temp_to_350_using_stub():
+    #     kitchen_stub = KitchenStub()  # collaborator
+    #     house = House(kitchen_stub, None)  # subject under test
+    #     house.bake_cookies()
+    #     assert_that(kitchen_stub.temp, equal_to(350))
+
     @staticmethod
-    def test_house_set_temp_to_350():
-        kitchen_stub = KitchenStub()
-        house = House(kitchen_stub, None)
+    def test_house_set_temp_to_350_using_mock():
+        kitchen_mock = mock()
+        house = House(kitchen_mock, None)  # subject under test
         house.bake_cookies()
-        assert_that(kitchen_stub.temp, equal_to(350))
+        verify(kitchen_mock).set_oven(350)
+        verify(kitchen_mock).set_oven(150)
+        verify(kitchen_mock).set_oven(650)
+        unstub()
